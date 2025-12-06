0f1f4fca06a4002e43f4391fddbfedd500df2649
Qalandarov Ibrohim <qalandarovi45@gmail.com>
2025-12-06 16:05:00 +0500

    Add files via upload

---
+++ funksiyalar.py
@@ -0,0 +1,80 @@
+# def salomlash(ism,fam):
+#     text = f"Assalomu alaykum hurmatli {ism} {fam} jamomizga hush kelibsiz!"
+#     return text
+# ism = input("Ibrohimjon: ").title()
+# fam = input("Qalandarov: ").title()
+# print(salomlash(ism,fam))
+
+#1-modul
+
+# #1-masala
+# def kvadrat(a):
+#     return a * a
+#
+# print(kvadrat(5))
+
+
+#2-masala
+# def takrorla():
+#     matn = input("Matnni kiriting: ")
+#     n = int(input("Necha marta takrorlash kerak "))
+#     print(matn * n)
+#
+# takrorla()
+
+
+#3-masala
+# def juftmi():
+#     son = int(input("Sonni kiriting: "))
+#
+#     if son % 2 == 0:
+#         print(True)
+#     else:
+#         print(False)
+#
+#
+# juftmi()
+
+
+#4-masala
+# def yigindi(a, b):
+#     return a + b
+# print("Yig'indi:", yigindi(int(input("Birinchi son: ")),
+# int(input("Ikkinchi son: "))))
+
+#5-masala
+
+# def uzunlik(matn):
+#     return len(matn)
+#
+# matn = input("Matn kiriting: ")
+# print("Matn uzunligi:", uzunlik(matn))
+
+#6-masala
+# def musbatmi(son):
+#     if son > 0:
+#         return "musbat"
+#     elif son < 0:
+#         return "manfiy"
+#     else:
+#         return "nol"
+#
+# son = float(input("Son kiriting: "))
+# print("Natija:", musbatmi(son))
+
+#7-masala
+# def modul(son):
+#     if son < 0:
+#         return -son
+#     else:
+#         return son
+#
+# son = float(input("Son kiriting: "))
+# print("Modul:", modul(son))
+
+#8-masala
+# def teskari(matn):
+#     return matn[::-1]
+#
+# print(teskari("salom"))  #
+# print(teskari("Python"))
